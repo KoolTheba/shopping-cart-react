4a4736cf93d54e685db04ff15616c15171e87e73
Theba Gomez <gomez.teba@gmail.com>
2020-10-13 12:24:13 +0200

    feat(test): Added tests dependencies, setup environment and basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import App from './App'
+import { shallow } from 'enzyme'
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  it('renders <App /> component correctly', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App')).toHaveLength(1)
+  });
 });
