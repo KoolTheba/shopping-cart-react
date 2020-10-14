e37bc3dead9c335f63baef0a9216f33bd1732789
Theba Gomez <gomez.teba@gmail.com>
2020-10-14 17:29:20 +0200

    feat(ui): Added interactions for Add Products section

--- src/AddProducts/index.js
+++ src/AddProducts/index.js
@@ -1,49 +1,33 @@
 import React from 'react'
 import './style.css'
-import data from '../data/products.json'
-console.log(data)
 
-export default class AddProducts extends React.Component {
+function AddProducts (props) {
 
-    state = {
-        products: []
-    }
-
-    componentDidMount(){
-        this.setState({ products: data })
-    }
-
-    handleAddProduct = (id) => {
-        console.log('ID-----', id)
-    }
-
-    render(){
-        const { products } = this.state
-
-        return(
-            <>
-            <h3>Cool products to add to your shopping list</h3>
-            <section className='products-list'>
-            {Object.entries(products).map(([id, info]) => (
-                <div 
-                    className="product-card"
-                    id={id}
-                    key={id}
-                >
-                    <div className="product-image">
-                        <img src={info.image} alt={info.image}/>
-                    </div>
-                    <div className="product-info">
-                        <p>{info.name}</p>
-                        <p>€{info.price}</p>
-                        <button
-                            onClick={() => this.handleAddProduct(id)}
-                        >Add to Cart</button>
-                    </div>
+    return(
+        <>
+        <h3>Cool products to add to your shopping list</h3>
+        <section className='products-list'>
+        {Object.entries(props.products).map(([id, info]) => (
+            <div
+                className="product-card"
+                id={id}
+                key={id}
+            >
+                <div className="product-image">
+                    <img src={info.image} alt={info.image}/>
                 </div>
-            ))}
-            </section>
-            </>
-        )
-    }
+                <div className="product-info">
+                    <p>{info.name}</p>
+                    <p>€{info.price}</p>
+                    <button
+                        onClick={() => props.addProducts(id)}
+                    >Add to Cart</button>
+                </div>
+            </div>
+        ))}
+        </section>
+        </>
+    )
 }
+
+export default AddProducts
