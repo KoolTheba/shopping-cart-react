a578bceff0dd9665aa6a761ba18bfbf0e9a54f74
Theba Gomez <gomez.teba@gmail.com>
2020-10-14 17:29:51 +0200

    feat(ui): Added logic to BuyProductsItem component

--- src/BuyProductItem/index.js
+++ src/BuyProductItem/index.js
@@ -1,11 +1,35 @@
 import React from 'react'
 import './style.css'
 
-function BuyProductItem () {
-    return(
-        <>
-        <h4>Items to buy</h4>
-        </>
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashAlt} from '@fortawesome/free-solid-svg-icons'
+
+function BuyProductItem (props) {
+    return (
+        <tr key={props.product.id}>
+            <td>
+            <button
+                onClick={() => props.removeListItem(props.product.id)}
+            ><FontAwesomeIcon icon={faTrashAlt}/></button></td>
+            <td>
+                <img
+                    className='product-image-table'
+                    src={props.product.image}
+                    alt={props.product.name}
+                />
+            {props.product.name}
+            </td>
+            <td>{props.product.price}€</td>
+            <button 
+                onClick={() => props.removeUnit(props.product.id)}
+                disabled={props.product.quantity > 1 ? false : true}
+            >-</button>
+            <td>{props.product.quantity}</td>
+            <button 
+                onClick={() => props.addUnit(props.product.id)}
+            >+</button>
+            <td>{props.product.price*props.product.quantity}€</td>
+        </tr>
     )
 }
 
