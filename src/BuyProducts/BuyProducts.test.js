e697c980439d171a53228a7d5c60f87f67faa965
Theba Gomez <gomez.teba@gmail.com>
2020-10-20 16:20:26 +0200

    feat(tests): Added BuyProducts component tests and some classes/ids for testing purposes

---
+++ src/BuyProducts/BuyProducts.test.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import BuyProducts from './'
+import { shallow, mount } from 'enzyme'
+
+const props = {
+    products: {
+        "PLANT14TGG": {
+            "name": "Zamioculcas",
+            "price": 90.00,
+            "stock": true,
+            "stockQ": 15,
+            "image": "https://image.shutterstock.com/image-photo/pot-zamioculcas-home-plant-on-600w-1399949501.jpg"
+        },
+    },
+    cartList: {
+        "PLANT14TGG": 2
+    },
+    removeListItem: jest.fn(),
+    removeUnit: jest.fn(),
+    addUnit: jest.fn(),
+    cleanShoppingList: jest.fn()
+}
+
+describe('BuyProducts component', () => {
+    it('renders BuyProducts component correctly', () => {
+        const wrapper = shallow(<BuyProducts {...props}/>)
+        expect(wrapper.find('#products-list-section')).toHaveLength(1)
+        expect(wrapper).toMatchSnapshot()
+    });
+
+    it('renders BuyProductsItem component correctly when there is at least 1 product and has props...', () => {
+        const productDetail = {
+            id: 'PLANT14TGG',
+            quantity: props.cartList['PLANT14TGG'],
+            image: props.products['PLANT14TGG'].image,
+            name: props.products['PLANT14TGG'].name,
+            price: props.products['PLANT14TGG'].price
+        }
+        const wrapper = mount(<BuyProducts {...props}/>)
+        expect(wrapper.find('BuyProductItem')).toHaveLength(1)
+        expect(wrapper.find('BuyProductItem').props().product).toEqual(productDetail)
+        expect(wrapper.find('BuyProductItem').props().addUnit).toEqual(props.addUnit)
+        expect(wrapper.find('BuyProductItem').props().removeUnit).toEqual(props.removeUnit)
+        expect(wrapper.find('BuyProductItem').props().removeListItem).toEqual(props.removeListItem)
+
+    });
+
+    it('renders CleanCart component correctly and has props...', () => {
+        const wrapper = mount(<BuyProducts {...props}/>)
+        expect(wrapper.find('CleanCart')).toHaveLength(1)
+        expect(wrapper.find('CleanCart').props().cleanShoppingList).toEqual(props.cleanShoppingList)
+    });
+});
+
